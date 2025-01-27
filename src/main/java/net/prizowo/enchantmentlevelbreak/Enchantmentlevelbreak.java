603513b8747abc49c4d4c94a93555f57435e4e80
xiaoliziawa <3682822069@qq.com>
2025-01-27 22:43:30 +0800

    Revert "2.0"
    This reverts commit 3b3917271a09a89af3700c38e0989e4f1d7d3073.

--- src/main/java/net/prizowo/enchantmentlevelbreak/Enchantmentlevelbreak.java
+++ src/main/java/net/prizowo/enchantmentlevelbreak/Enchantmentlevelbreak.java
@@ -1,25 +1,22 @@
 package net.prizowo.enchantmentlevelbreak;
 
-import net.neoforged.bus.api.IEventBus;
+
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
-import net.prizowo.enchantmentlevelbreak.config.Config;
 
-@Mod(Enchantmentlevelbreak.MODID)
+@Mod("enchantmentlevelbreak")
 public class Enchantmentlevelbreak {
     public static final String MODID = "enchantmentlevelbreak";
 
-    public Enchantmentlevelbreak(ModContainer modContainer, IEventBus modEventBus) {
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-        
+    public Enchantmentlevelbreak() {
         NeoForge.EVENT_BUS.register(this);
     }
+
     @SubscribeEvent
     public void onRegisterCommands(RegisterCommandsEvent event) {
         CEnchantCommand.register(event.getDispatcher());
     }
+
 }
